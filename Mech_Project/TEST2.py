1bea30ac57c962097568bc294147412833990fc8
Mohan <satbin123@gmail.com>
2023-04-09 19:52:19 +0530

    changes to TEST2

--- Mech_Project/TEST2.py
+++ Mech_Project/TEST2.py
@@ -5,7 +5,7 @@ def f(x):
     return x**2 + 2*x + 1
 
 # Use the derivative function to compute the derivative
-dfdx = spm.derivative(f, 2.0, dx=1e-6)
+dfdx = spm.derivative(f, 3.0, dx=1e-6)
 
 # Print the result
-print("The derivative of f(x) at x = 2.0 is:", dfdx)
+print("The derivative of f(x) at x = 3.0 is:", dfdx)
